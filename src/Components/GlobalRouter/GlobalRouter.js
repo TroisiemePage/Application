89c96576520a49d8f053da5d7bbb92377588f624
StrowBeary <remicaillot5@gmail.com>
2018-05-18 15:57:09 +0200

    global router

--- src/Components/GlobalRouter/GlobalRouter.js
+++ src/Components/GlobalRouter/GlobalRouter.js
@@ -1,18 +1,88 @@
 import * as React from "react";
-import {View} from "react-native";
+import {Button, Dimensions, View} from "react-native";
 import {PageRouter} from "../PageRouter/PageRouter";
+import Menu from "../Menu/Menu";
+
+const {height, width} = Dimensions.get('window');
 
 export class GlobalRouter extends React.Component {
+
+
+    state = {
+        page: "MENU"
+    };
 
     constructor() {
         super();
     }
 
     render() {
-        return (
-            <View>
-                <PageRouter/>
-            </View>
-        );
+        let view = (<View/>);
+        switch (this.state.page) {
+            case "BOOK":
+                view = (<PageRouter/>);
+                break;
+            case "DICTIONNARY":
+                view = (
+                    <View style={{
+                        position: "absolute",
+                        top: 20,
+                        left: 20,
+                        flex: 1,
+                        flexDirection: "row",
+                        alignItems: 'center',
+                    }}>
+                        <Button
+                            onPress={() => {
+                            }}
+                            title="Menu"
+                        />
+                    </View>
+                );
+                break;
+            case "MENU":
+                view = [<Menu key={1}/>, (
+                    <View key={2} style={{
+                        position: "absolute",
+                        top: 20,
+                        left: 20,
+                        flex: 1,
+                        flexDirection: "row",
+                        alignItems: 'center',
+                    }}>
+                        <Button
+                            onPress={() => {
+                            }}
+                            title="Menu"
+                        />
+                    </View>
+                )];
+                break;
+            default:
+                view = [<Menu key={1}/>, (
+                    <View key={2} style={{
+                        position: "absolute",
+                        top: 20,
+                        left: 20,
+                        flex: 1,
+                        flexDirection: "row",
+                        alignItems: 'center',
+                    }}>
+                        <Button
+                            onPress={() => {
+                            }}
+                            title="Menu"
+                        />
+                    </View>
+                )];
+                break;
+        }
+        return (<View style={{
+            width: width,
+            height: height,
+            backgroundColor: '#FEFBEB'
+        }}>
+            {view}
+        </View>);
     }
 }
