925158ba4bd82c7a2e86fceb1117e8571747a186
popo <miss.poepoe23@gmail.com>
2018-06-11 01:28:57 +0200

    textes popup page 1

--- src/Components/Pages/Page1.js
+++ src/Components/Pages/Page1.js
@@ -36,63 +36,61 @@ export class Page1 extends React.Component {
                         resizeMethod={"scale"}
                     />
                     <ModalView
-                        x={150}
+                        x={140}
                         y={380}
-                        title="ABBAYE ROYALE DE FONTEVRAUD"
+                        title="ABBÉ BENOÎT"
                         popupImage={Clerc}
                     >
-                        Au Moyen Âge, les paysans forment les neuf dixièmes de la population.
-                        On distingue deux sortes de paysans au Moyen Âge : les serfs et les paysans libres (les
-                        vilains). Les serfs appartiennent au seigneur qui a donc tous les droits sur eux tandis que les
-                        vilains doivent travailler pour lui et lui payer des impôts.
-                        Les gens modestes avaient des vêtements assez semblables entre eux. La robe courte était le
-                        costume ordinaire des paysans avec les chaussures à la poulaine, autrement dit celles ayant un
-                        bout pointu et relevé. Ils portaient le plus souvent les couleurs les moins nobles comme le
-                        gris, obtenu à l'aide d'écorce d'aulne, un arbre poussant en milieu humide.
+                        Il est venu accompagné de l’évêque Aubertin pour baptiser le nouveau prince. Il fait parti du clergé qui constitue  l’ensemble des personnes religieuses.  Dans le monde chrétien du Moyen-Âge, il existait deux genres de clergé :
+                        {"\n"}{"\n"}
+                        Le clergé séculier{"\n"}
+                        Il est composé des prêtres (curés et évêques). Ils vivent au milieu du peuple. Ils enseignent, organisent la messe et sont à l'écoute des gens.
+                        {"\n"}{"\n"}
+                        Le clergé régulier{"\n"}
+                        Le clergé régulier comprend les moines et les nonnes, les religieux et religieuses. Ils vivent séparés des autres hommes. Ils ont des activités manuelles, écrivent des manuscrits et suivent des offices religieux. Ils vivent dans une abbaye, un monastère ou un couvent et doivent suivre un règlement strict qui organise leurs journées.
+                        {"\n"}{"\n"}
+                        Certains moines s'habillaient avec des vêtements en laine. La robe traditionnelle s'appelle la coule. Par dessus, les frères portaient une longue tunique noire.
+
                     </ModalView>
                     <ModalView
                         x={405}
                         y={250}
-                        title="ABBAYE ROYALE DE FONTEVRAUD"
+                        title="HUGON ET GERSINDE "
                         popupImage={Paysans}
                     >
-                        Au Moyen Âge, les paysans forment les neuf dixièmes de la population.
-                        On distingue deux sortes de paysans au Moyen Âge : les serfs et les paysans libres (les
-                        vilains). Les serfs appartiennent au seigneur qui a donc tous les droits sur eux tandis que les
-                        vilains doivent travailler pour lui et lui payer des impôts.
-                        Les gens modestes avaient des vêtements assez semblables entre eux. La robe courte était le
-                        costume ordinaire des paysans avec les chaussures à la poulaine, autrement dit celles ayant un
-                        bout pointu et relevé. Ils portaient le plus souvent les couleurs les moins nobles comme le
-                        gris, obtenu à l'aide d'écorce d'aulne, un arbre poussant en milieu humide.
+                        Serf du seigneur Anguerrand, Hugon cultive ses terres. Gersinde, fille du charpentier, est une paysanne libre. Ils profitent d’un événement royal pour festoyer.
+                        {"\n"}{"\n"}
+                        Au Moyen Âge, les paysans forment les neuf dixièmes de la population. On en distingue deux sortes : les serfs et les paysans libres (les vilains). Les serfs appartiennent au seigneur qui a donc tous les droits sur eux tandis que les vilains doivent travailler pour lui et lui payer des impôts.
+                        {"\n"}{"\n"}
+                        Les gens modestes avaient des vêtements assez semblables entre eux. La robe courte était le costume ordinaire des paysans avec les chaussures à la poulaine, autrement dit celles ayant un bout pointu et relevé. Ils portaient le plus souvent les couleurs les moins nobles comme le gris, obtenu à l'aide d'écorce d'aulne, un arbre poussant en milieu humide.
                     </ModalView>
                     <ModalView
-                        x={685}
+                        x={672}
                         y={300}
-                        title="ABBAYE ROYALE DE FONTEVRAUD"
+                        title="MONSEIGNEUR ANGUERRAND ET DAME HERMELINE "
                         popupImage={Nobles}
                     >
-                        Au Moyen Âge, les paysans forment les neuf dixièmes de la population.
-                        On distingue deux sortes de paysans au Moyen Âge : les serfs et les paysans libres (les
-                        vilains). Les serfs appartiennent au seigneur qui a donc tous les droits sur eux tandis que les
-                        vilains doivent travailler pour lui et lui payer des impôts.
-                        Les gens modestes avaient des vêtements assez semblables entre eux. La robe courte était le
-                        costume ordinaire des paysans avec les chaussures à la poulaine, autrement dit celles ayant un
-                        bout pointu et relevé. Ils portaient le plus souvent les couleurs les moins nobles comme le
-                        gris, obtenu à l'aide d'écorce d'aulne, un arbre poussant en milieu humide.
+                        Ils se sont rencontrés lors d’une joute et fêtent aujourd’hui leurs fiançailles. Il a gagné sa main en combattant aux côtés de Grandgousier, car les nobles appartiennent à la classe des guerriers.
+                        {"\n"}{"\n"}
+                        La grande noblesse détient les terres tandis que la petite noblesse garde des parcelles de terre en échange de services militaires et d’argent.
+                        {"\n"}{"\n"}
+                        Dans la vie de tous les jours, ils représentent les administrateurs et les fonctionnaires de la société : ils s’occupent de leurs terres, de l’application de la loi, du commandement des militaires et des tribunaux.
+                        {"\n"}{"\n"}
+                        Les nobles s'habillaient avec des collants, des tuniques et des vêtements qui étaient plus coûteux, comme de la fourrure (par exemple de la fourrure d'hermine) ou de la soie, et des teintures chères. Il y avait également beaucoup de parures et autres bijoux sur leurs vêtements pour montrer leur supériorité au peuple de pouvoir posséder de telles choses. Les couleurs principales étaient le bleu et le rouge.
                     </ModalView>
                     <ModalView
                         x={635}
                         y={30}
-                        title="ABBAYE ROYALE DE FONTEVRAUD"
+                        title="À VOS ORDRES !"
                     >
-                        Au Moyen Âge, les paysans forment les neuf dixièmes de la population.
-                        On distingue deux sortes de paysans au Moyen Âge : les serfs et les paysans libres (les
-                        vilains). Les serfs appartiennent au seigneur qui a donc tous les droits sur eux tandis que les
-                        vilains doivent travailler pour lui et lui payer des impôts.
-                        Les gens modestes avaient des vêtements assez semblables entre eux. La robe courte était le
-                        costume ordinaire des paysans avec les chaussures à la poulaine, autrement dit celles ayant un
-                        bout pointu et relevé. Ils portaient le plus souvent les couleurs les moins nobles comme le
-                        gris, obtenu à l'aide d'écorce d'aulne, un arbre poussant en milieu humide.
+                        La société médiévale se structure en trois ordres :
+                        {"\n"}{"\n"}
+                        ORATORES : ceux qui prient, les hommes d'Église
+                        {"\n"}{"\n"}
+                        BELLATORES : ceux qui font la guerre, les nobles (prince, seigneurs, chevaliers)
+                        {"\n"}{"\n"}
+                        LABORATORES : ceux qui travaillent, les paysans
+
                     </ModalView>
 
                 </View>
