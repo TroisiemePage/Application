79657edf05132fc6d5e2db03e0c856ddbd3b637d
popo <miss.poepoe23@gmail.com>
2018-06-12 14:43:29 +0200

    paysans2

--- src/Components/Pages/Page1/Page1.js
+++ src/Components/Pages/Page1/Page1.js
@@ -6,7 +6,7 @@ import {words} from "../../../Stores/words";
 import ModalView from '../../Modal/ModalPopup/ModalPopupView';
 
 import Nobles from "../../../Assets/Images/Pages/Page1/nobles.png";
-import Paysans from "../../../Assets/Images/Pages/Page1/paysans.png";
+import Paysans from "../../../Assets/Images/Pages/Page1/paysans2.png";
 import Clerc from "../../../Assets/Images/Pages/Page1/clerc.png";
 import {Sky} from "./Sky";
 
